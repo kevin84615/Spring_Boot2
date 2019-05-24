f85c2486e368422dc3a4cba1c9c8052c82c97c45
user <kevin84615@gmail.com>
2019-05-24 11:55:21 +0900

    画面の遷移

--- MyBootApp/src/main/java/com/example/demo/controller/HelloController.java
+++ MyBootApp/src/main/java/com/example/demo/controller/HelloController.java
@@ -11,62 +11,82 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
-	
-	@RequestMapping(value = {"/"}, method = {RequestMethod.GET})
-	public ModelAndView index() {
-		
-		// 生成
-		ModelAndView mv = new ModelAndView();
-		
-		// テンプレートを指定
-		mv.setViewName("index");
-		
-		// 日時を取得、設定
-		mv.addObject("now", new Date().toString());
-		
-		// 返却
-		return mv;
-	}
-	
-	// POST用のパラメータを受け取る
-	@RequestMapping(value = {"/formPost"}, method = {RequestMethod.POST})
-	public ModelAndView postTest1(
-			@RequestParam(value="name", required = true) String name,
-			@RequestParam(value="age", required = true) String age) {
-		
-		// 生成
-		ModelAndView mv = new ModelAndView();
-		
-		// テンプレートを指定
-		mv.setViewName("post");
-		
-		// modelに設定して画面に表示するようにする
-		mv.addObject("name", name);
-		mv.addObject("age", age);
-		
-		// 返却
-		return mv;
-	}
-	
-	// GET用のパラメータを受け取る
-	@RequestMapping(value = {"/formPost"}, method = {RequestMethod.GET})
-	public ModelAndView getTest1(
-			@RequestParam(value="name", required = true) String name,
-			@RequestParam(value="age", required = true) String age) {
-		
-		// 生成
-		ModelAndView mv = new ModelAndView();
-		
-		// テンプレートを指定
-		mv.setViewName("post");
-		
-		// modelに設定して画面に表示するようにする
-		mv.addObject("name", name);
-		mv.addObject("age", age);
-		
-		// 返却
-		return mv;
-	}
-	
-}
 
+	@RequestMapping(value = { "/" }, method = { RequestMethod.GET })
+	public ModelAndView index() {
+
+		ModelAndView mv = new ModelAndView();
+
+		mv.setViewName("index");
+
+		mv.addObject("now", new Date().toString());
+
+		return mv;
+	}
+
+	// POST用のパラメータを受け取る
+	@RequestMapping(value = { "/formPost" }, method = { RequestMethod.POST })
+	public ModelAndView postTest1(@RequestParam(value = "user_id", required = true) String user_id,
+			@RequestParam(value = "password", required = true) String password) {
+
+		ModelAndView mv = new ModelAndView();
+
+		mv.setViewName("post");
+
+		// modelに設定して画面に表示するようにする
+		mv.addObject("user_id", user_id);
+		mv.addObject("password", password);
+
+		return mv;
+	}
+
+	// GET用のパラメータを受け取る
+	@RequestMapping(value = { "/formPost" }, method = { RequestMethod.GET })
+	public ModelAndView getTest1(@RequestParam(value = "user_id", required = true) String user_id,
+			@RequestParam(value = "password", required = true) String password) {
+
+		ModelAndView mv = new ModelAndView();
+
+		mv.setViewName("post");
+
+		// modelに設定して画面に表示するようにする
+		mv.addObject("user_id", user_id);
+		mv.addObject("password", password);
+
+		return mv;
+	}
+
+	// POST用のパラメータを受け取る
+	@RequestMapping(value = { "/new" }, method = { RequestMethod.POST })
+	public ModelAndView postTest2() {
+
+		ModelAndView mv = new ModelAndView();
+
+		mv.setViewName("new");
+
+		return mv;
+	}
+
+	// GET用のパラメータを受け取る
+	@RequestMapping(value = { "/new" }, method = { RequestMethod.GET })
+	public ModelAndView getTest2() {
+
+		ModelAndView mv = new ModelAndView();
+
+		mv.setViewName("new");
+
+		return mv;
+	}
+
+	// POST用のパラメータを受け取る
+	@RequestMapping(value = { "/result" }, method = { RequestMethod.POST })
+	public ModelAndView postTest3() {
+
+		ModelAndView mv = new ModelAndView();
+
+		mv.setViewName("result");
+
+		return mv;
+	}
+
+}
