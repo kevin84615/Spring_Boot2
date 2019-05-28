005cf92cf15b02a6fe3f5e838cede5903f369f11
user <kevin84615@gmail.com>
2019-05-28 15:17:48 +0900

    Date update

--- Spring_Boot_2/src/main/java/com/tuyano/springboot/HeloController.java
+++ Spring_Boot_2/src/main/java/com/tuyano/springboot/HeloController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import javax.annotation.PostConstruct;
 import com.tuyano.springboot.repositories.MyDataRepository;
+import java.util.Optional;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HeloController {
@@ -31,6 +33,24 @@ public class HeloController {
 	@Transactional(readOnly=false)
 	public ModelAndView form(
 			@ModelAttribute("formModel") MyData mydata, 
+			ModelAndView mav) {
+		repository.saveAndFlush(mydata);
+		return new ModelAndView("redirect:/");
+	}
+	
+	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+	public ModelAndView edit(@ModelAttribute MyData mydata, 
+			@PathVariable int id,ModelAndView mav) {
+		mav.setViewName("edit");
+		mav.addObject("title","edit mydata.");
+		Optional<MyData> data = repository.findById((long)id);
+		mav.addObject("formModel",data.get());
+		return mav;
+	}
+
+	@RequestMapping(value = "/edit", method = RequestMethod.POST)
+	@Transactional(readOnly=false)
+	public ModelAndView update(@ModelAttribute MyData mydata, 
 			ModelAndView mav) {
 		repository.saveAndFlush(mydata);
 		return new ModelAndView("redirect:/");
--- Spring_Boot_2/src/main/java/com/tuyano/springboot/repositories/MyDataRepository.java
+++ Spring_Boot_2/src/main/java/com/tuyano/springboot/repositories/MyDataRepository.java
@@ -2,10 +2,14 @@ package com.tuyano.springboot.repositories;
 
 import com.tuyano.springboot.MyData;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MyDataRepository  extends JpaRepository<MyData, Long> {
+	
+	public Optional<MyData> findById(Long name);
 	
 }
